1a2111cba7ed3e5f300962216281baf46c39482c
Benny Cohen <benny@mediamagic.co.il>
2012-11-26 00:22:17 +0200

    init repo

---
+++ js/app.js
@@ -0,0 +1,20 @@
+'use strict';
+angular.module('sketchApp', ['ngResource', 'ui']).
+	config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+	$routeProvider.
+		when('/main', {templateUrl: 'views/Main.html', controller: GeneralCtrl, name:'Main'}).
+		when('/about', {templateUrl: 'views/About.html', controller: AboutCtrl, name: 'About'}).
+		when('/gallery', {templateUrl: 'views/Gallery.html', controller: GalleryCtrl, name:'Gallery'}).
+		otherwise({redirectTo: '/main'});
+	//$locationProvider.html5Mode(true);
+}]).directive('openDialog', function(){
+    return {
+        restrict: 'A',
+        link: function(scope, elem, attr, ctrl) {
+            var dialogId = '#' + attr.openDialog;
+            elem.bind('click', function(e) {
+                $(dialogId).dialog('open');
+            });
+        }
+    };
+});
---
+++ js/controllers.js
@@ -0,0 +1,99 @@
+'use strict';
+
+/*
+ * Main Controller
+ * create a global scope with routing information
+**/
+function MainCtrl($scope, $route, $routeParams, $location) {
+  $scope.$route = $route;
+  $scope.$location = $location;
+  $scope.$routeParams = $routeParams;
+  $scope.route = $route;
+}
+
+/*
+ * General Controller
+ * Controlling general settings
+**/
+function GeneralCtrl($scope,$resource){
+	$scope.Settings = $resource('//gi.mediamagic.co.il/clients/avoda/stickers/resources/settings', {});
+	$scope.settings = $scope.Settings.get(function(r){ 
+		$scope.settings = r;
+		$scope.currentTemplate = $scope.settings.templates[0];
+		$scope.Api = $resource($scope.settings.host + 'api/:action', {});
+		$scope.Rest = $resource($scope.settings.host + 'resources/:collection/:id', {});
+	});
+
+	$scope.user = {
+		token: 0,
+		userId: 0
+	};
+
+	$scope.formData = {
+		imageText: {posX: 0, posY: 0 },
+		imageTemplate: 0
+	};
+
+	$scope.imageData = {};
+	
+	$scope.selectTemplate = function(index){
+		$scope.currentTemplate = angular.copy($scope.settings.templates[index]);
+	}
+
+	$scope.save = function(){
+		if ($scope.user.token == 0) {
+			$('#loginDialog').dialog('open');
+		} else
+		$scope.sendImage($scope.formData);
+	}
+
+	$scope.$on('loginUser', function(e, a){
+		$scope.user = a;
+		$('#loginDialog').dialog('close');
+		$scope.sendImage($scope.formData);
+	});
+
+	$scope.sendImage = function(_obj){
+		_obj.token = $scope.user.token;
+		$scope.Rest.save({collection: 'image'}, _obj, function(response){
+			$scope.imageData = response;
+			$('#imageDialog').dialog('open');
+		});
+	}
+}
+
+function UserCtrl($scope,$resource){
+	$scope.loginData = {};
+	$scope.registerData = {};
+	$scope.error = false;
+	$scope.login = function(){
+		//$scope.loginData.password = jQuery.sha1($scope.loginData.password);
+		$scope.Api.save({action: 'login'}, $scope.loginData, function(response){
+			if (response.error) {
+				$scope.error = true;
+				$scope.errorMsg = response.description;
+				return;
+			}
+			$scope.$emit('loginUser', response);
+		});
+	}
+
+	$scope.registerData = {};
+	$scope.register = function(){
+		console.log($scope.registerData);
+		$scope.Rest.save({collection: 'user'}, $scope.registerData, function(response){
+			console.log(response);
+		});
+	}
+}
+
+function ImageCtrl($scope,$resource){
+}
+
+function AboutCtrl($scope){
+
+}
+
+function GalleryCtrl($scope,$resource){
+
+}
