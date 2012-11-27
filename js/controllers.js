be428d0039ed0f010afb6ca0bef9847f1f4cb905
Benny Cohen <benny@mediamagic.co.il>
2012-11-28 00:27:29 +0200

    removed unneeded files

--- js/controllers.js
+++ js/controllers.js
@@ -5,24 +5,24 @@
  * create a global scope with routing information
 **/
 function MainCtrl($scope, $route, $routeParams, $location) {
-  $scope.$route = $route;
-  $scope.$location = $location;
-  $scope.$routeParams = $routeParams;
-  $scope.route = $route;
+	$scope.$route = $route;
+	$scope.$location = $location;
+	$scope.$routeParams = $routeParams;
+	$scope.route = $route;
 }
 
 
-function HomeCtrl($scope){
-	
+function HomeCtrl($scope) {
+	//controller stuff
 }
 
 /*
  * General Controller
  * Controlling general settings
 **/
-function GeneralCtrl($scope,$resource){
+function GeneralCtrl($scope, $resource) {
 	$scope.Settings = $resource('//gi.mediamagic.co.il/clients/avoda/stickers/resources/settings', {});
-	$scope.settings = $scope.Settings.get(function(r){ 
+	$scope.settings = $scope.Settings.get(function (r) {
 		$scope.settings = r;
 		$scope.currentTemplate = $scope.settings.templates[0];
 		$scope.Api = $resource($scope.settings.host + 'api/:action', {});
@@ -40,40 +40,40 @@ function GeneralCtrl($scope,$resource){
 	};
 
 	$scope.imageData = {};
-	
-	$scope.selectTemplate = function(index){
+	$scope.selectTemplate = function (index) {
 		$scope.currentTemplate = angular.copy($scope.settings.templates[index]);
-	}
+	};
 
-	$scope.save = function(){
-		if ($scope.user.token == 0) {
+	$scope.save = function () {
+		if ($scope.user.token === 0) {
 			$('#loginDialog').dialog('open');
-		} else
-		$scope.sendImage($scope.formData);
-	}
+		} else {
+			$scope.sendImage($scope.formData);
+		}
+	};
 
-	$scope.$on('loginUser', function(e, a){
+	$scope.$on('loginUser', function (e, a) {
 		$scope.user = a;
 		$('#loginDialog').dialog('close');
 		$scope.sendImage($scope.formData);
 	});
 
-	$scope.sendImage = function(_obj){
-		_obj.token = $scope.user.token;
-		$scope.Rest.save({collection: 'image'}, _obj, function(response){
+	$scope.sendImage = function (obj) {
+		obj.token = $scope.user.token;
+		$scope.Rest.save({collection: 'image'}, obj, function (response) {
 			$scope.imageData = response;
 			$('#imageDialog').dialog('open');
 		});
-	}
+	};
 }
 
-function UserCtrl($scope,$resource){
+function UserCtrl($scope, $resource) {
 	$scope.loginData = {};
 	$scope.registerData = {};
 	$scope.error = false;
-	$scope.login = function(){
+	$scope.login = function () {
 		//$scope.loginData.password = jQuery.sha1($scope.loginData.password);
-		$scope.Api.save({action: 'login'}, $scope.loginData, function(response){
+		$scope.Api.save({action: 'login'}, $scope.loginData, function (response) {
 			if (response.error) {
 				$scope.error = true;
 				$scope.errorMsg = response.description;
@@ -81,24 +81,20 @@ function UserCtrl($scope,$resource){
 			}
 			$scope.$emit('loginUser', response);
 		});
-	}
+	};
 
 	$scope.registerData = {};
-	$scope.register = function(){
+	$scope.register = function () {
 		console.log($scope.registerData);
-		$scope.Rest.save({collection: 'user'}, $scope.registerData, function(response){
+		$scope.Rest.save({collection: 'user'}, $scope.registerData, function (response) {
 			console.log(response);
 		});
-	}
+	};
 }
 
-function ImageCtrl($scope,$resource){
+function ImageCtrl($scope, $resource) {
 }
 
-function AboutCtrl($scope){
-
-}
-
-function GalleryCtrl($scope,$resource){
+function GalleryCtrl($scope, $resource) {
 
 }
